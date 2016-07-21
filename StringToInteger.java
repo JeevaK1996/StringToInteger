0d032d601677f0d71a708bdf73a32068f17c4914
JeevaK1996 <jeeva.kcse@ksrce.ac.in>
2016-07-21 15:04:55 +0530

    Create StringToInteger.java

---
+++ StringToInteger.java
@@ -0,0 +1,14 @@
+# StringToInteger
+
+public class StringToInteger {
+
+	
+	public static void main(String[] args) {
+			String a="13232";
+			int b=Integer.valueOf(a);
+			System.out.println(b);
+		
+
+	}
+
+}
